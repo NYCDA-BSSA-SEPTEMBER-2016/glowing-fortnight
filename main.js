6b84dcaf31a3061d924cc9e7b0409f6820fd8cd4
Jimmy Voskuil <jimmyvoskuil@msn.com>
2016-10-04 17:28:31 +0200

    added smooth scrolling and some text

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ $("#text").fadeIn(3000).fadeOut(2000, function(){
   console.log('text faded out')
     $("#texttext").fadeIn(2000);
 });
+
+
+$('.nav a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top -50
+    }, 500);
+    return false;
+});
